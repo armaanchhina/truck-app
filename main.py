c21d5c9537206f37d55f3bd532ec6f40ecf5f187
armaanchhina <armaan_45@hotmail.com>
2023-07-05 07:34:35 -0700

    added error handling in trucking_database.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 import pymysql.cursors
 from trucking_database import insert_new_tractor_info, insert_repair_info, get_repair_info
+import pandas as pd
+import io
+app = Flask(__name__, template_folder='public', static_folder='public/static')
+# app = Flask(__name__, template_folder='public')
 
-# app = Flask(__name__, template_folder='public', static_folder='public/static')
-app = Flask(__name__, template_folder='public')
 
 
 @app.route('/')
@@ -15,9 +17,15 @@ def insert_tractor():
     asset_id = request.form.get('assetID')
     vin = request.form.get('vin')
     inspection_date = request.form.get('inspectionDate')
+    licence_plate = request.form.get('licencePlate')
+    make = request.form.get('make')
+    model = request.form.get('model')
+    axle = request.form.get('axle')
+    last_tire_replace_date = request.form.get('lastTireReplaceDate')
+    print(type(last_tire_replace_date))
 
     # Connect to the database and insert the new tractor information
-    # Be sure to handle errors and edge cases
+    insert_new_tractor_info(asset_id, vin, inspection_date, licence_plate, make, model, axle, last_tire_replace_date)
 
     return "Tractor information inserted successfully!", 200
 
@@ -28,7 +36,7 @@ def insert_repair():
     repair_date = request.form.get('repairDate')
     cost = request.form.get('cost')
     repair_type = request.form.get('repairType')
-
+    insert_repair_info(repair_id, repair_asset_id, repair_date, cost, repair_type)
     # Connect to the database and insert the new repair information
     # Be sure to handle errors and edge cases
 
@@ -41,11 +49,26 @@ def get_repair():
 
     # Connect to the database and get the repair information
     # Be sure to handle errors and edge cases
+    print(repair_info_asset_id)
+    df = get_repair_info(repair_info_asset_id)
+    output = io.BytesIO()
+    print(df)
+
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
+    writer.save()
+    output.seek(0)
+    return send_file(output, attachment_filename='repair_info.xlsx', as_attachment=True)
 
     return "Repair information retrieved successfully!", 200
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET'])
 def login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def handle_login():
     username = request.form.get('username')
     password = request.form.get('password')
     
--- trucking_database.py
+++ trucking_database.py
@@ -1,4 +1,6 @@
 import pymysql.cursors
+import pandas as pd
+import io
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
@@ -7,31 +9,41 @@ connection = pymysql.connect(host='localhost',
                              cursorclass=pymysql.cursors.DictCursor)
 
 
-# try:
-#     with connection.cursor() as cursor:
-#         sql = "INSERT INTO tractor_info (assetId, vin, total_repair_costs, inspection_date) VALUES (%s, %s, %s, %s)"
-#         cursor.execute(sql, (1, 123456, 1000, '2023-05-30'))
-        
-        
+def get_db_connection(username, password):
+    connection = pymysql.connect(host='localhost',
+                                 user=username,
+                                 password=password,
+                                 database='tractor_database',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    return connection
 
-#     # connection is not autocommit by default. So you must commit to save
-#     # your changes.
-#     connection.commit()
-
-
-# finally:
-#     connection.close()
-
-
-def insert_new_tractor_info(assetId: int, inspection_date: str = None, vin: int = 0, total_repair_costs: int = 0 ):
-    # SQL query to insert into tractor_info table
+def insert_new_tractor_info(assetId: int, vin: int, inspection_date: str, licence_plate: str, make: str, model: str, axle: str, last_tire_replace_date: str):
     try:
         with connection.cursor() as cursor:
-            sql = "INSERT INTO tractor_info (assetId, vin, total_repair_costs, inspection_date) VALUES (%s, %s, %s, %s)"
-            cursor.execute(sql, (assetId, vin, total_repair_costs, inspection_date))
+            # Check if the assetId already exists
+            sql = "SELECT * FROM tractor_info WHERE assetId = %s"
+            cursor.execute(sql, (assetId,))
+            result = cursor.fetchone()
+
+            if result:
+                # If the assetId exists, perform an UPDATE
+                sql = """
+                UPDATE tractor_info 
+                SET vin = %s, inspection_date = %s, licence_plate = %s, make = %s, model = %s, axle = %s, last_tire_replace_date = %s
+                WHERE assetId = %s
+                """
+                cursor.execute(sql, (vin, inspection_date, licence_plate, make, model, axle, last_tire_replace_date, assetId))
+            else:
+                # If the assetId does not exist, perform an INSERT
+                sql = "INSERT INTO tractor_info (assetId, vin, inspection_date, licence_plate, make, model, axle, last_tire_replace_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(sql, (assetId, vin, inspection_date, licence_plate, make, model, axle, last_tire_replace_date))
+            
             connection.commit()
+    except Exception as e:
+        return(f"An error occurred: {e}")
     finally:
-            connection.close()
+        connection.close()
+
 
 def delete_tractor_info(assetId: int):
     try:
@@ -39,28 +51,54 @@ def delete_tractor_info(assetId: int):
             sql = "DELETE FROM tractor_info WHERE assetId = %s"
             cursor.execute(sql, (assetId,))
             connection.commit()
+    except Exception as e:
+        return(f"An error occurred: {e}")
     finally:
-            connection.close()
+        connection.close()
 
-
-def insert_repair_info(repair_id: int, assetId: int, repair: str, repair_date: str, cost: float, repair_type: str):
-    # SQL query to insert into Repairs table
+def insert_repair_info(repair_id: int, assetId: int, repair_date: str, cost: float, repair_type: str):
+    # Connect to the database
     try:
         with connection.cursor() as cursor:
+            # First, check if the assetId exists in the tractor_info table
+            sql = "SELECT * FROM tractor_info WHERE assetId = %s"
+            cursor.execute(sql, (assetId,))
+            result = cursor.fetchone()
+
+            # If the assetId does not exist in the tractor_info table, insert it
+            if result is None:
+                sql = "INSERT INTO tractor_info (assetId) VALUES (%s)"
+                cursor.execute(sql, (assetId,))
+                connection.commit()
+
+            # Now you can insert into the Repairs table
             sql = "INSERT INTO Repairs (repairId, assetId, Repair_Date, Cost, Repair_Type) VALUES (%s, %s, %s, %s, %s)"
             cursor.execute(sql, (repair_id, assetId, repair_date, cost, repair_type))
             connection.commit()
+    except Exception as e:
+        return(f"An error occurred: {e}")
     finally:
         connection.close()
+
 
 def get_repair_info(assetId: int):
     try:
         with connection.cursor() as cursor:
-            sql = "SELECT * FROM Repairs WHERE assetID  = %s"
-            cursor.execute(sql, (assetId,))
+            if(assetId):
+                sql = """SELECT r.*, t.* FROM Repairs r 
+                         JOIN tractor_info t ON r.assetID = t.assetId 
+                         WHERE r.assetID  = %s"""
+                cursor.execute(sql, (assetId,))
+            else:
+                sql = """SELECT r.*, t.* FROM Repairs r 
+                         JOIN tractor_info t ON r.assetID = t.assetId"""
+                cursor.execute(sql)
+            result = cursor.fetchall()
+            df = pd.DataFrame(result)
     finally:
-        result = cursor.fetchall()
         connection.close()
+        return df
+
 
 
 # insert_new_tractor_info(3)
